563f86430812a05a1e2bc48ba7c8ad9c28cd8dc9
Pierre TRETON <pierre.treton@etudiant.univ-rennes1.fr>
2023-12-29 10:44:49 +0100

    maj loading

--- src/app/display-component/display-component.component.ts
+++ src/app/display-component/display-component.component.ts
@@ -12,13 +12,15 @@ export class DisplayComponentComponent implements OnInit {
   
   // the Pokemon displayed 
   model: PokeModel | undefined;
+  loading: boolean = true;
 
   constructor(private api: PokeApiService, private selectedService: PokeSelectionService) {}
   
   ngOnInit(): void {
     // subscribe to the service observable
-    this.selectedService.getId().subscribe((value) => this.handleIdChange(value))
-    this.selectedService.setId(1);
+    this.loading = true;
+    setTimeout(() => this.loading = false, 2000);
+    this.selectedService.id.subscribe((value) => this.handleIdChange(value))
   }
 
   /**
@@ -26,9 +28,13 @@ export class DisplayComponentComponent implements OnInit {
    * @param value the new value
    */
   handleIdChange(value: number): void {
-    if(value !== -1) {
+    this.model = undefined;
+    this.loading = true;
+    setTimeout(() => this.loading = false, 2000);
+    if(value !== -1) {  
       this.api.getPokemonDetails(value).subscribe((data: any) => {
         this.model = data;
+        this.loading = false;
       });
     }
   }
